e7adb442d80b1b09b1289d8d768ef1844af596f4
willy <willy@asus>
2018-11-03 13:08:54 +0700

    version 0.4

--- RPL/src/java/Service/ApplicationConfig.java
+++ RPL/src/java/Service/ApplicationConfig.java
@@ -29,7 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(Service.PaketResource.class);
+        resources.add(Service.KurirResource.class);
     }
     
 }
---
+++ RPL/src/java/Service/KurirResource.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import com.google.gson.Gson;
+import helper.kurirHelper;
+import java.util.List;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pojos.Kurir;
+
+/**
+ * REST Web Service
+ *
+ * @author willy
+ */
+@Path("kurir")
+public class KurirResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of KurirResource
+     */
+    public KurirResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of Service.KurirResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJson() {
+        kurirHelper helper = new kurirHelper();
+        List<Kurir> list = helper.bacaSemuaKurir();
+        
+        Gson g = new Gson();
+        String json = g.toJson(list);
+        return Response.status(200)
+                .entity(json)
+                .build();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of KurirResource
+     *
+     * @param content representation for the resource
+     */
+//    @PUT
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public void putJson(String content) {
+//    }
+    @POST
+    @Path("addKurir")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addNewDokter(String data) {
+        Gson gson = new Gson();
+        Kurir kurir = gson.fromJson(data, Kurir.class);
+        kurirHelper helper = new kurirHelper();
+        helper.addNewKurir(
+                kurir.getNama(),
+                kurir.getAlamat(),
+                kurir.getStatus(),
+                kurir.getNoTelp(),
+                kurir.getAgama(),
+                kurir.getTanggalLahir(),
+                kurir.getNik()
+        );
+
+        return Response
+                .status(200)
+                .entity(kurir)
+                .build();
+    }
+
+}
